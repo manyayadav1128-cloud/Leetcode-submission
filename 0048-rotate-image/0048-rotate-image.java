1e8b0c051b07f5ccdac3372132872559bfc6f43c
manyayadav1128-cloud <manyayadav1128@gmail.com>
2026-09-22 00:16:11 +0530

    Time: 0 ms (100%), Space: 43.6 MB (70.65%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length ; i++){
+            for (int j = i+1; j< matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp; 
+            }
+        }
+        for(int row=0; row < matrix.length; row++){
+                int startCol =0;
+                int endCol= matrix.length - 1;
+                while(startCol <=endCol){
+                    int temp = matrix[row][startCol];
+                    matrix[row][startCol] = matrix[row][endCol];
+                    matrix[row][endCol]=temp;
+                    startCol++;
+                    endCol--;
+                }
+           }
+        }
+
+        
+    }
